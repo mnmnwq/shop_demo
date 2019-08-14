40e5c7278bf576a75fa3c413351e1f101f388d65
www <mnmnwq@qq.com>
2019-08-14 16:41:18 +0800

    aa

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -30,7 +30,7 @@ class WechatController extends Controller
         //解析XML
         $xml = simplexml_load_string($data,'SimpleXMLElement', LIBXML_NOCDATA);        //将 xml字符串 转换成对象
         $xml = (array)$xml; //转化成数组
-        echo "<pre>";print_r($xml);
+        //echo "<pre>";print_r($xml);
         \Log::Info(json_encode($xml));  //输出收到的信息
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents(storage_path('logs/wx_event.log'),$log_str,FILE_APPEND);
